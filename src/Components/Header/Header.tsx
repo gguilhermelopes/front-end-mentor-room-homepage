a36347df719d70dd34d033fc8df6697882a33197
gguilhermelopes <guilhermelopes275@gmail.com>
2023-03-05 20:26:09 -0300

    style: mobile adjustments

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,27 +1,32 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { ReactComponent as Logo } from "../../Helpers/images/logo.svg";
+import useMobile from "../../Hooks/useMobile";
 
 const Header = () => {
+  const mobile = useMobile("(max-width:1100px)");
+
   return (
     <header className={styles.header}>
       <Logo />
-      <nav>
-        <ul className={styles.ul}>
-          <li>
-            <a href="/">home</a>
-          </li>
-          <li>
-            <a href="/">shop</a>
-          </li>
-          <li>
-            <a href="/">about</a>
-          </li>
-          <li>
-            <a href="/">contact</a>
-          </li>
-        </ul>
-      </nav>
+      {!mobile && (
+        <nav>
+          <ul className={styles.ul}>
+            <li>
+              <a href="/">home</a>
+            </li>
+            <li>
+              <a href="/">shop</a>
+            </li>
+            <li>
+              <a href="/">about</a>
+            </li>
+            <li>
+              <a href="/">contact</a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
--- src/Components/Main/MainSlides.tsx
+++ src/Components/Main/MainSlides.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { useState } from "react";
 import Button from "../../Helpers/Button";
 import left from "../../Helpers/images/icon-angle-left.svg";
 import right from "../../Helpers/images/icon-angle-right.svg";
 import image1 from "../../Helpers/images/desktop-image-hero-1.jpg";
+import imageMobile1 from "../../Helpers/images/mobile-image-hero-1.jpg";
+import imageMobile2 from "../../Helpers/images/mobile-image-hero-2.jpg";
+import imageMobile3 from "../../Helpers/images/mobile-image-hero-3.jpg";
 import image2 from "../../Helpers/images/desktop-image-hero-2.jpg";
 import image3 from "../../Helpers/images/desktop-image-hero-3.jpg";
 import styles from "./MainSlides.module.css";
+import useMobile from "../../Hooks/useMobile";
 
 const slides = [
   {
@@ -13,6 +17,7 @@ const slides = [
     description:
       "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life.  collection and make your property a reflection of you and what you love.",
     image: image1,
+    imageMobile: imageMobile1,
     alt: "Four white chairs made of wood and a table with a bonsai in a room",
   },
   {
@@ -20,6 +25,8 @@ const slides = [
     description:
       "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     image: image2,
+    imageMobile: imageMobile2,
+
     alt: "Three chairs on a simple room",
   },
   {
@@ -27,12 +34,15 @@ const slides = [
     description:
       "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
     image: image3,
+    imageMobile: imageMobile3,
     alt: "Dark chair on a room with gray wall",
   },
 ];
 
 const MainSlides = () => {
-  const [slide, setSlide] = React.useState(0);
+  const [slide, setSlide] = useState<number>(0);
+  const mobile = useMobile("(max-width:1100px)");
+  const mobileImage = useMobile("(max-width:900px)");
 
   function next() {
     if (slide < slides.length - 1) setSlide(slide + 1);
@@ -44,19 +54,38 @@ const MainSlides = () => {
 
   return (
     <div className={styles.mainSlides}>
-      <img src={slides[slide].image} alt={slides[slide].alt} />
+      {mobile ? (
+        <div className={styles.imageMobileWrapper}>
+          <img
+            src={mobileImage ? slides[slide].imageMobile : slides[slide].image}
+            alt={slides[slide].alt}
+          />
+          <div className={styles.buttonWrapper}>
+            <Button onClick={before} disabled={slide === 0}>
+              <img src={left} alt="Before button" />
+            </Button>
+            <Button onClick={next} disabled={slide === slides.length - 1}>
+              <img src={right} alt="Next button" />
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <img src={slides[slide].image} alt={slides[slide].alt} />
+      )}
       <div className={styles.slideInfo}>
         <h1>{slides[slide].title}</h1>
         <p>{slides[slide].description}</p>
         <a href="/">Shop Now</a>
-        <div className={styles.buttonWrapper}>
-          <Button onClick={before} disabled={slide === 0}>
-            <img src={left} alt="Before button" />
-          </Button>
-          <Button onClick={next} disabled={slide === slides.length - 1}>
-            <img src={right} alt="Next button" />
-          </Button>
-        </div>
+        {!mobile && (
+          <div className={styles.buttonWrapper}>
+            <Button onClick={before} disabled={slide === 0}>
+              <img src={left} alt="Before button" />
+            </Button>
+            <Button onClick={next} disabled={slide === slides.length - 1}>
+              <img src={right} alt="Next button" />
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
